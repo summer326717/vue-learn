e7ebcfe987eb819d1efdb72f2229cf09309740e4
HGROAPJ2KXW6PW4\Administrator <yintj@100emall.com>
2017-06-29 15:09:38 +0800

    add vue demo

---
+++ src/components/Hello.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="block">
+      <span class="demonstration">默认显示颜色</span>
+      <span class="wrapper">
+        <el-button type="success">成功按钮</el-button>
+        <el-button type="warning">警告按钮</el-button>
+        <el-button type="danger">危险按钮</el-button>
+        <el-button type="info">信息按钮</el-button>
+      </span>
+    </div>
+    <div class="block">
+      <span class="demonstration">hover 显示颜色</span>
+      <span class="wrapper">
+        <el-button :plain="true" type="success">成功按钮</el-button>
+        <el-button :plain="true" type="warning">警告按钮</el-button>
+        <el-button :plain="true" type="danger">危险按钮</el-button>
+        <el-button :plain="true" type="info">信息按钮</el-button>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
